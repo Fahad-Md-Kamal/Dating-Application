72a3bee9ad98be3202377224eb3efd6c1f48be4b
Fahad-Md-Kamal <faahad.hossain@gmail.com>
2019-06-13 19:40:18 +0600

    Initial Source Controle

---
+++ DatingApp.API/DatingApplication/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using DatingApplication.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApplication.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions<DataContext> options) : base(options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
