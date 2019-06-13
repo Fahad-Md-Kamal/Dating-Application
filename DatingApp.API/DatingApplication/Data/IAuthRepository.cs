0501d174f37a7d1f640171197828939e6ded17b2
Fahad-Md-Kamal <faahad.hossain@gmail.com>
2019-06-14 01:30:55 +0600

    Authentication added

---
+++ DatingApp.API/DatingApplication/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using DatingApplication.Models;
+
+namespace DatingApplication.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
+    }
+}
--- DatingApp.API/DatingApplication/Startup.cs
+++ DatingApp.API/DatingApplication/Startup.cs
@@ -28,9 +28,10 @@ namespace DatingApplication
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
+            
+            services.AddScoped<IAuthRepository, AuthRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
