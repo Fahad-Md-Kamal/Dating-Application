2d09760c39de1148ae63b2f7ebc1dc28140b4d5d
Fahad-Md-Kamal <faahad.hossain@gmail.com>
2019-06-13 20:20:58 +0600

    UserClassCreated

--- DatingApp.API/DatingApplication/Migrations/20190613140801_Added Users.Designer.cs
+++ DatingApp.API/DatingApplication/Migrations/20190613140801_Added Users.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DatingApplication.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DatingApplication.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190613140801_Added Users")]
+    partial class AddedUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
