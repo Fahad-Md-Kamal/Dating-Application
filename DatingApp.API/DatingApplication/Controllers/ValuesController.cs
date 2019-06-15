073f86de100e768c2374eb743bded3d3d10ecea9
Fahad-Md-Kamal <faahad.hossain@gmail.com>
2019-06-15 17:20:07 +0600

    Components communication implemented with client Authentication

--- DatingApp.API/DatingApplication/Controllers/ValuesController.cs
+++ DatingApp.API/DatingApplication/Controllers/ValuesController.cs
@@ -22,6 +22,7 @@ namespace DatingApplication.Controllers
         }
 
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
