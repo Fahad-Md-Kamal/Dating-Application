f0fd3bd11e60a62a062202aa1a9ddabd687bf270
Fahad-Md-Kamal <faahad.hossain@gmail.com>
2019-06-26 17:29:37 +0600

    UserUpdate from api and UI done

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -7,6 +7,9 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemeberDetailComponent } from './members/memeber-detail/memeber-detail.component';
 import { MemberDetailResolver } from './_resolver/member-detail.resolver';
 import { MemberListResolver } from './_resolver/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolver/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export const appRoutes: Routes = [
     {path: '', component: HomeComponent},
@@ -15,8 +18,12 @@ export const appRoutes: Routes = [
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard],
         children: [
-            {path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
-            {path: 'members/:id', component: MemeberDetailComponent, resolve: {user: MemberDetailResolver}},
+            {path: 'members', component: MemberListComponent,
+                resolve: {users: MemberListResolver}},
+            {path: 'members/:id', component: MemeberDetailComponent,
+                resolve: {user: MemberDetailResolver}},
+            {path: 'member/edit', component: MemberEditComponent,
+                resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges]},
             {path: 'messages', component: MessagesComponent},
             {path: 'lists', component: ListsComponent}
         ]
